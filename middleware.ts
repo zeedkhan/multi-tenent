db24bcbe7fce58947175b01f7f15b5eb2a809abd
tanakit <zeedkhan.tp@gmail.com>
2023-03-24 10:16:10 +0800

    main

--- middleware.ts
+++ middleware.ts
@@ -35,7 +35,7 @@ export default async function middleware(req: NextRequest) {
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname
-          .replace(`.vercel.pub`, "")
+          .replace(`.multi-tenent`, "")
           .replace(`.platformize.vercel.app`, "")
       : hostname.replace(`.localhost:3000`, "");
 
