b80f1b8464b71bb1b0793649a6b2dc6bf73ac0ea
tanakit <zeedkhan.tp@gmail.com>
2023-03-24 12:59:32 +0800

    add domain in nextAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -38,7 +38,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? ".vercel.pub" : undefined,
+        domain: VERCEL_DEPLOYMENT ? ".multi-tenent.vercel.app" : undefined,
         secure: VERCEL_DEPLOYMENT,
       },
     },
