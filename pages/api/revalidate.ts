6004e43bed4fd2712db7b5558bedc81516da7b8d
tanakit <zeedkhan.tp@gmail.com>
2023-03-24 14:48:50 +0800

    test

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -5,7 +5,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { urlPath } = req.body;
-  res.setHeader("Access-Control-Allow-Origin", "https://app.vercel.pub");
+  res.setHeader("Access-Control-Allow-Origin", "https://app.multi-tenent.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "POST");
 
   try {
